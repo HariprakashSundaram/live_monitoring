e5af0db1fbd89e84d2af45461919c4d8e7825335
Hariprakash2025 <hariromil@gmail.com>
2025-09-04 00:25:23 +0530

    Add older files from legacy_code folder

---
+++ push.bat
@@ -0,0 +1,6 @@
+git init
+git add .
+git commit -m "Add older files from legacy_code folder"
+git branch -M main
+git remote add origin https://github.com/HariprakashSundaram/live_monitoring.git
+git push -u origin main
