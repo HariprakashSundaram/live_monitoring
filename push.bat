aced3556ddbc9ef27c9de813c9b7209b75c89979
Hariprakash2025 <hariromil@gmail.com>
2025-09-04 09:59:16 +0530

    commited on 04-9-2025

--- push.bat
+++ push.bat
@@ -1,6 +1,6 @@
 git init
 git add .
-git commit -m "Add older files from legacy_code folder"
+git commit -m "commited on 04-9-2025"
 git branch -M main
 git remote add origin https://github.com/HariprakashSundaram/live_monitoring.git
 git push -u origin main
